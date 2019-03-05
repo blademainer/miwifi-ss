d26b46577617f66a0b4986ef69a3af9a2713d01f
blademainer <blademainer@gmail.com>
2019-03-05 23:24:52 +0800

    Update mini_ss.sh

--- mini/mini_ss.sh
+++ mini/mini_ss.sh
@@ -12,7 +12,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 cd /tmp
 rm -f shadowsocks_mini.tar.gz
-curl https://raw.githubusercontent.com/jk2K/miwifi-ss/master/mini/shadowsocks_mini.tar.gz -o shadowsocks_mini.tar.gz
+curl https://raw.githubusercontent.com/blademainer/miwifi-ss/master/mini/shadowsocks_mini.tar.gz -o shadowsocks_mini.tar.gz
 tar zxf shadowsocks_mini.tar.gz
 
 # install shadowsocks ss-redir to /userdisk/sysapihttpd/shadowsocks/bin
